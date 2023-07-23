1a08f3c56fcbd34d2a0b037f8923b5c7beaacfec
TechDanny <kokidanny15@gmail.com>
2023-07-23 06:12:34 -0700

    sort array in ascending order using selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
 	{
 		return;
 	}
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		smaller = i;
 		for (j = i + 1; j < size; j++)
@@ -26,8 +26,8 @@ void selection_sort(int *array, size_t size)
 		}
 			if (i != smaller)
 			{
-				temp = array[j];
-				array[j] = array[smaller];
+				temp = array[i];
+				array[i] = array[smaller];
 				array[smaller] = temp;
 				print_array(array, size);
 			}
