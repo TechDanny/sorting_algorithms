bf7dfbae662581bc104a4a6de7028a460f930544
TechDanny <kokidanny15@gmail.com>
2023-07-23 03:14:35 -0700

    print the array in ascending order using bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integer in ascending order
+ * @array:list of integers
+ * @size:number of elements in the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
