45eddc5c43a795fd1c33797a7274cecbe0d0ad74
TechDanny <kokidanny15@gmail.com>
2023-07-25 03:20:28 -0700

    main header file

--- sort.h
+++ sort.h
@@ -25,6 +25,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void lomuto_sorting(int *array, size_t size, int low, int high);
+int lomuto_divide(int *array, size_t size, int low, int high);
+void swap_values(int *m, int *n);
 
 
 #endif
